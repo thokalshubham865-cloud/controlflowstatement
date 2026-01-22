8c91130bc5082150c0a266c77b352379b9c3297e
Shubham Thokal <thokalshubham865@gmail.com>
2026-01-22 15:32:52 +0530

    Add files via upload

---
+++ MainClassJava.java
@@ -0,0 +1,26 @@
+
+public class MainClassJava {
+
+	public static void main (String []arggs) {
+	
+	          Car bmw = new Car ();
+	          bmw.carRunning();
+	          bmw.carStarting();
+	          bmw.carWheels();
+	          
+	          System.out.println();
+	
+	          Car audi = new Car();
+	          audi.carRunning();
+	          audi.carStarting();
+	          audi.carWheels();
+	          
+	          System.out.println();
+	          
+	          Car SSSS = new Car ();
+	          SSSS.carRunning();
+	          SSSS.carStarting();
+	          SSSS.carWheels();
+	      
+	  }
+   }
